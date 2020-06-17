f1c8c7253020613839d218c9b594cca9dc40ffcf
Dan Tran <dantr.qld@gmail.com>
2020-06-17 22:23:39 +1000

    Added Python folder

---
+++ test.py
@@ -0,0 +1,35 @@
+import random
+
+
+for x in range(4):
+     
+    print('Time: '+ str(x)) 
+    #declare variable a and variable b
+    a = random.randrange(1,1000)  #declare a as a random number
+    b = random.randrange(1,1000)  #declare b as a random number
+
+
+
+    print('a value is '+ str(a))
+    print('b value is '+ str(b))
+
+    if a>b:
+        print('a is greater than b')
+        
+    else:
+        print('b is greater than a')
+    
+
+
+
+
+
+
+
+
+
+    
+
+    
+
+
