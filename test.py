06d9704c2ff1dc2f8f3cf74559219ad04125d3ae
Dan Tran <dantr.qld@gmail.com>
2020-06-24 03:03:57 +1000

    change file content

--- test.py
+++ test.py
@@ -4,12 +4,18 @@ import random
 for x in range(4):
      
     print('Time: '+ str(x)) 
+    print('__________________________')
     #declare variable a and variable b
 
     for x in range(0,10):
         a = random.randrange(1,100)  #declare a as a random number
         b = random.randrange(1,100)  #declare b as a random number
-        print a+b
+        print (a + b)
+
+    
+
+
+
 
     
 
